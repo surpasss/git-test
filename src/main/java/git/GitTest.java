766ee6f7004cea8913e7e7db015a440a8665f1e2
zyj <975266353@qq.com>
2022-06-29 16:14:47 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,14 @@
+package git;
+
+/**
+ * @author： ZYJ
+ * @Description:
+ * @date: 2022-06-29 16:10
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
